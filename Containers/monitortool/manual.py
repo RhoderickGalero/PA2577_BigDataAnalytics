323a5b52b1dbd27cc31a0b53a8fee7dd45ae527e
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2024-01-24 20:33:59 +0100

    chang

--- Containers/monitortool/manual.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb://dbstorage:27017")
-db = client.cloneDetector
-statistics = db.statistics.find()
-for stat in statistics:
-    print(stat)
-client.close()
